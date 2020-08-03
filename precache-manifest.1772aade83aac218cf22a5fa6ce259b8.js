103cb5657819706849de5d2a898d71ac2408153c
Franco <tovar.franco@hotmail.com>
2020-08-03 18:32:41 -0300

    Updates

---
+++ precache-manifest.1772aade83aac218cf22a5fa6ce259b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d36bd5f16154e83fef87e3c1fa6246",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2c1cb6296d965f2a9454",
+    "url": "/my-app/static/css/main.09dac9c1.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/my-app/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1cb6296d965f2a9454",
+    "url": "/my-app/static/js/main.4002dcd3.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
